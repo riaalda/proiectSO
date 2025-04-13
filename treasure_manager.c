9886b480b71869d9212906855b59417b8da47ee6
Alda Rianna <aldarianna08@gmail.com>
2025-04-13 22:59:51 +0300

    Finished the tasks for phase 1: added the symlink implementation and some more validations in the addTreasure function

--- treasure_manager.c
+++ treasure_manager.c
@@ -36,15 +36,77 @@ void addLog(const char *huntID, const char *message) {
     char logPath[PATH_SIZE];
     snprintf(logPath, sizeof(logPath), "%s/logged_hunt.log", huntID);
 
-    FILE *logFile = fopen(logPath, "a");
-    if (!logFile) {
+
+    // open log file
+    int fd = open(logPath, O_WRONLY | O_CREAT | O_APPEND, 0666);
+    if (fd == -1) {
         perror("Could not open log file");
         return;
     }
 
-    fprintf(logFile, "%s\n", message);
-    fclose(logFile);
+    // verification fro log file
+    size_t messageLen = strlen(message);
+    if (write(fd, message, messageLen) != messageLen) {
+        perror("Failed to write message to log file");
+        close(fd);
+        return;
+    }
+
+    // newline failure
+    if (write(fd, "\n", 1) != 1) {
+        perror("Failed to write newline to log file");
+        close(fd);
+        return;
+    }
+
+    close(fd);
+
+    // huntId validation -> if it contains slashed, the symlink does not work
+    if (strchr(huntID, '/') != NULL) {
+        fprintf(stderr, "Invalid hunt Id! It must not contain '/' character!\n");
+        return;
+    }
+
+    // create symlink in the current directory
+    char symlinkName[PATH_SIZE];
+    snprintf(symlinkName, sizeof(symlinkName), "logged_hunt-%s", huntID);
+
+    // absolute path to the log file
+    char absoluteLogPath[PATH_SIZE];
+    if (realpath(logPath, absoluteLogPath) == NULL) {
+        perror("Failed to resolve absolute path for log file!\n");
+        return;
+    }
+
+    struct stat linkStat;
+    if (lstat(symlinkName, &linkStat) == -1) {
+        // create the symlink (it does not exist yet)
+        if (symlink(absoluteLogPath, symlinkName) == -1) {
+            perror("Failure at creating the symlink for log file!\n");
+        }
+    } else {
+        // symlinkul exists -> verification step
+        char existingTarget[PATH_SIZE];
+        ssize_t len = readlink(symlinkName, existingTarget, sizeof(existingTarget) - 1);
+        if (len != -1) {
+            existingTarget[len] = '\0';
+            if (strcmp(existingTarget, absoluteLogPath) != 0) {
+                fprintf(stderr, "Symlink exists but points to wrong target! Attempt to recreate it!\n");
+                if (unlink(symlinkName) == -1) {
+                    perror("Failed to remove incorrect symlink!\n");
+                    return;
+                }
+                if (symlink(absoluteLogPath, symlinkName) == -1) {
+                    perror("Failed to recreate symlink!\n");
+                }
+            }
+        } else {
+            perror("Failed to read existing symlink!\n");
+        }
+    }
 }
+
+
 
 int addTreasure(Treasure *t, const char *huntID) {
     char dirPath[PATH_SIZE], treasureFile[PATH_SIZE];
@@ -52,7 +114,7 @@ int addTreasure(Treasure *t, const char *huntID) {
     snprintf(treasureFile, sizeof(treasureFile), "%s/%s_treasures.dat", huntID, huntID); // writes the treasure in the file treasures.dat (binary file)
 
     if (mkdir(dirPath, 0777) == -1 && errno != EEXIST) {
-        perror("mkdir failed!\n");
+        perror("Mkdir verification failed!\n");
         return 0;
     } // verification for creating a folder
 
@@ -62,19 +124,67 @@ int addTreasure(Treasure *t, const char *huntID) {
         return 0;
     }
 
+    /*
     printf("Enter Treasure ID: ");
     scanf("%d", &t->id);
-    getchar(); 
+    getchar();*/
+
+    // verification if the id is unique
+    int isUnique = 0;
+    while (!isUnique) {
+        printf("Enter Treasure ID: ");
+        scanf("%d", &t->id);
+        getchar(); 
+
+        // verification step
+        int fd_check = open(treasureFile, O_RDONLY);
+        if (fd_check != -1) {
+            Treasure existing;
+            isUnique = 1; 
+            while (read(fd_check, &existing, sizeof(Treasure)) == sizeof(Treasure)) {
+                if (existing.id == t->id) {
+                    printf("Error: A treasure with ID %d already exists in this hunt! Please enter a different ID.\n", t->id);
+                    isUnique = 0; //id not unique
+                    break;
+                }
+            }
+            close(fd_check);
+        } else {
+            perror("Error while opening the treasure File for checking if the ID is unique!\n");
+            close(fd);
+            return 0;
+        }
+    }
 
     printf("Enter Username: ");
     fgets(t->username, USERNAME_SIZE, stdin);
     t->username[strcspn(t->username, "\n")] = 0;
 
-    printf("Enter Latitude: ");
+    /*printf("Enter Latitude: ");
     scanf("%f", &t->latitude);
     printf("Enter Longitude: ");
     scanf("%f", &t->longitude);
-    getchar();
+    getchar();*/
+
+    // verification if the GPS coordinates are valid
+    int isValid = 0;
+    while (!isValid) {
+      printf("Enter Latitude (-90 to 90): ");
+      scanf("%f", &t->latitude);
+
+      printf("Enter Longitude (-180 to 180): ");
+      scanf("%f", &t->longitude);
+      getchar(); // input buffer
+
+      if (t->latitude >= -90.0 && t->latitude <= 90.0 &&
+	  t->longitude >= -180.0 && t->longitude <= 180.0) {
+        isValid = 1; // valid coordinates
+      } else {
+        printf("Error: Invalid GPS coordinates! Latitude must be between -90 and 90 and longitude between -180 and 180!\n");
+        printf("Please enter the coordinates again!\n");
+      }
+    }
+
 
     printf("Enter Clue: ");
     fgets(t->clue, CLUE_SIZE, stdin);
@@ -85,7 +195,7 @@ int addTreasure(Treasure *t, const char *huntID) {
 
     // creates the file treasure
     if (write(fd, t, sizeof(Treasure)) != sizeof(Treasure)) {
-        perror("Failed to write treasure to file");
+        perror("Failed to write treasure to file!");
         close(fd);
         return 0;
     }
@@ -268,7 +378,7 @@ void removeHunt(const char *huntID) {
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
-        perror("Wrong number of arguments given!\nCorrect format that should be given: treasure_manager <flag>\nPossible flags:\n--add <hunt id>\n--list <hunt id>\n--view <hunt id> <id>\n--remove_treasure <hunt id> <id>\n--remove_hunt <hunt id>\n");
+        perror("Wrong number of arguments given!\nCorrect format that should be given: treasure_manager <option>\nPossible options:\n--add <hunt id>\n--list <hunt id>\n--view <hunt id> <id>\n--remove_treasure <hunt id> <id>\n--remove_hunt <hunt id>\n");
         exit(1);
     }
 
@@ -276,7 +386,7 @@ int main(int argc, char *argv[]) {
 
     if (strcmp(argv[1], "--add") == 0) {
         if (argc != 3) {
-            perror("You should enter a hunt name\n");
+            perror("You should enter a hunt name (--add <hunt_name>)\n");
             exit(1);
         }
         if (treasure == NULL) {
@@ -290,14 +400,14 @@ int main(int argc, char *argv[]) {
     }
     else if (strcmp(argv[1], "--list") == 0) {
         if (argc != 3) {
-            perror("You should enter a hunt name\n");
+            perror("You should enter a hunt name (--list <hunt_name>)\n");
             exit(1);
         }
         listTreasures(argv[2]);
     }
     else if (strcmp(argv[1], "--view") == 0) {
         if (argc != 4) {
-            perror("You should enter a hunt name and a specific treasure you want to see\n");
+            perror("You should enter a hunt name and a specific treasure you want to see (--view <hunt_name> <treasure_id>)\n");
             exit(1);
         }
         int id = atoi(argv[3]);
@@ -305,7 +415,7 @@ int main(int argc, char *argv[]) {
     }
     else if (strcmp(argv[1], "--remove_treasure") == 0) {
         if (argc != 4) {
-            perror("You should enter a hunt name and the treasure ID to remove\n");
+            perror("You should enter a hunt name and the treasure ID to remove (--remove_treasure <hunt_name> <treasure_id>\n");
             exit(1);
         }
         int id = atoi(argv[3]);
@@ -313,13 +423,13 @@ int main(int argc, char *argv[]) {
     }
     else if (strcmp(argv[1], "--remove_hunt") == 0) {
         if (argc != 3) {
-            perror("You should enter the name of a hunt you want to remove\n");
+            perror("You should enter the name of a hunt you want to remove (--remove_hunt <hunt_name>\n");
             exit(1);
         }
         removeHunt(argv[2]);
     }
     else {
-        printf("You've entered an unknown command!\n");
+        printf("Unknown command entered!\n");
     }
 
     return 0;
